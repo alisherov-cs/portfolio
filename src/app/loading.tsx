29b44870fcab35a03ea0e9aef358d66df769cd26
alisherov-cs <alisherov.cs@gmail.com>
2024-07-17 17:28:27 +0500

    feat: done

---
+++ src/app/loading.tsx
@@ -0,0 +1,7 @@
+export default function Loading() {
+    return (
+        <main className="w-screen h-screen bg-black text-white flex itecems-center justify-center">
+            <h1>Loading...</h1>
+        </main>
+    );
+}
---
+++ src/app/not-found.tsx
@@ -0,0 +1,7 @@
+export default function NotFound() {
+    return (
+        <main className="w-screen h-screen bg-black text-white flex itecems-center justify-center">
+            <h1>404 - Page Not Found</h1>
+        </main>
+    );
+}
